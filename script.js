49ca3506ce96e906c93d5dc630db365706f510c0
Jordan Roberts <jxavier831@gmail.com>
2022-06-28 15:57:48 -0400

    Initial JavaScript finished. Initial Project finished. Something in JavaScript file causing BMI form not to work. Will have to debug.

--- script.js
+++ script.js
@@ -5,3 +5,55 @@ let male = document.getElementById("m");
 let female = document.getElementById("f");
 let form = document.getElementById("form");
 
+validateForm = () => {
+    if (age.value == '' || height.value == '' || weight.value == '' ||
+    (male.checked == false && female.checked == false)) {
+        alert("All fields are required!");
+        document.getElementById("submit").removeEventListener("click", countBmi);
+    } else {
+        countBmi();
+    }
+}
+
+document.getElementById("submit").addEventListener("click", validateForm);
+
+countBmi = () => {
+    let p = [age.value, height.value, weight.value];
+    if(male.checked){
+        p.push("male");
+    } else if(female.checked) {
+        p.push("female");
+    }
+}
+
+form.reset();
+    let bmi = Number(p[2])/(Number(p[1])/100*Number(p[1])/100);
+    let result = '';
+    if (bmi < 18.5) {
+        result = 'Underweight';
+    } else if (18.5 <= bmi && bmi <= 24.9){
+        result = 'Healthy';
+    } else if (25 <= bmi && bmi <= 29.9){
+        result = 'Overweight';
+    } else if (30 <= bmi && bmi <=34.9){
+        result = 'Obese';
+    } else if (35 <= bmi) {
+        result = 'Extremely Obese'
+    }
+
+    let h1 = document.createElement("h1");
+    let h2 = document.createElement("h2");
+    let t = document.createTextNode(result);
+    let b = document.createTextNode('BMI: ');
+    let r = document.createTextNode(parseFloat(bmi).toFixed(2));
+    h1.appendChild(t);
+    h2.appendChild(b);
+    h2.appendChild(r);
+    document.body.appendChild(h1);
+    document.body.appendChild(h2);
+    document.getElementById("submit").removeEventListener("click", countBmi);
+    document.getElementById("submit").removeEventListener("click", validateForm);
+
+document.getElementById("submit").addEventListener("click", countBmi);
+    
+    
